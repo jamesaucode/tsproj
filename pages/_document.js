f572101c30fbfa2b96e00c1251d17e2e54584b69
James Au <jamesaubudi@gmail.com>
2019-07-01 20:07:31 -0400

    style changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <title>Study App</title>
+        <meta charSet="utf-8" />
+        <meta name="author" content="James Au" />
+        <meta name="description" content="This is a web application for students to create study flashcards and share with people." />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
         <link rel="shortcut icon" type="image/png" href="../static/images/favicon.ico"/>
         <link rel="stylesheet" href="../static/reset.css" />
         <link rel="stylesheet" href="../static/custom.css" />
